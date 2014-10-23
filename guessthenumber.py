87e26f981680c031b146d1433575a780191ebb4c
Brenton Strine <brenton.strine@gmail.com>
2014-10-22 17:32:23 -0700

    Feature complete

--- guessthenumber.py
+++ guessthenumber.py
@@ -1,36 +1,48 @@
 import simplegui
 import random
 secret_number = 0
+upper_bound = 100
+guesses_left = 0
 
 def new_game():
-    global secret_number
-    secret_number = random.randrange(0,100)
+    global secret_number, upper_bound, guesses_left
+    secret_number = random.randrange(0,upper_bound)
+    if(upper_bound==100):    
+        guesses_left = 7
+    else:
+        guesses_left = 10
+    
+    print "\n", "New Game!", "\n"
 
 # define event handlers for control panel
 def range100():
-    # button that changes the range to [0,100) and starts a new game 
-    
-    # remove this when you add your code    
-    pass
+    global upper_bound
+    upper_bound = 100
+    new_game()
 
 def range1000():
-    # button that changes the range to [0,1000) and starts a new game     
-    
-    pass
+    global upper_bound
+    upper_bound = 1000
+    new_game()
     
 def input_guess(guess):
+    global guesses_left
     guess = int(guess)
     print "Guess was ", guess
+    guesses_left = guesses_left - 1
+    print "Number of guesses left: ", guesses_left
     if(secret_number > guess):
         print "Higher"
     elif(secret_number < guess):
         print "Lower"
     elif(secret_number == guess):
-        new_game()
         print "Correct"
+        new_game()
     else:
         print "Error!"
-
+    if(guesses_left == 0):
+        print "You're out of guesses! You lose!"
+        new_game()
     
 # create frame
 f = simplegui.create_frame("Guess the Number",300,300)
@@ -38,6 +50,8 @@ f = simplegui.create_frame("Guess the Number",300,300)
 
 # register event handlers for control elements and start frame
 f.add_input("Your Guess", input_guess, 100)
+f.add_button("Range: 0 - 100", range100, 100)
+f.add_button("Range: 0 - 1000", range1000, 100)
 
 
 # call new_game 
