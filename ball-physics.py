ed0d5dfd63b14cc501833f106d449199bb05eb53
Brenton Strine <brenton.strine@gmail.com>
2014-10-24 16:10:39 -0700

    Cleaning up a little
    Tons of mess from trying to fix bounce issue getting cleaned up.

--- ball-physics.py
+++ ball-physics.py
@@ -14,37 +14,21 @@ max = 0
 
 # define event handlers
 def keydown(key):
-    vel = 4
+    global vel
     if key == simplegui.KEY_MAP["left"]:
-        ball_pos[0] -= vel
+        vel[0] -= 1
     elif key == simplegui.KEY_MAP["right"]:
-        ball_pos[0] += vel
+        vel[0] += 1
     elif key == simplegui.KEY_MAP["down"]:
-        ball_pos[1] += vel
+        vel[1] += 1
     elif key == simplegui.KEY_MAP["up"]:
-        ball_pos[1] -= vel   
+        vel[1] -= 1   
         
 def draw(canvas):
-    global max
-    max += 1
-    if(max%100==1):
-        print "starti: " + str(vel[1])
     # Gravity and air resistance
     vel[0] = vel[0] / 1.005
-    #weird = (1 + (random.random()/1))
-    #vel[1] = (vel[1] + .8) / weird
     vel[1] = (vel[1] + .8) / 1.005
-    
-    if(max%100==1):
-        print "gravit: " + str(vel[1])
 
-    
-
-    #vel[1] = math.floor(vel[1] * 1000) / 1000
-    
-    
-    
-    
     # collide and reflect off of left hand side of canvas
     if ball_pos[0] <= BALL_RADIUS:
        ball_pos[0] = BALL_RADIUS
@@ -60,17 +44,12 @@ def draw(canvas):
         
     if ball_pos[1] >= HEIGHT - 1 -BALL_RADIUS:
        ball_pos[1] = HEIGHT - 1 -BALL_RADIUS
-       if(vel[0]>0):
-        vel[0] -= .8
-       if(vel[0]<0):
-        vel[0] += .8
+       vel[1] = - (vel[1] / 1.1)
          
        if(vel[1] <= 1 and vel[1] >= -1):
         vel[1] = vel[1] / 2
        if(vel[1] <= .4 and vel[1] >= -.4):
         vel[1] = 0
-       if(max%100==1):
-            print "bounce: " + str(vel[1])
         
     # Update ball position
     ball_pos[0] += vel[0]
