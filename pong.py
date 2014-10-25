887e0cb2e465e93dbbfbabf2a65558efcae137ec
Brenton Strine <brenton.strine@gmail.com>
2014-10-24 18:57:46 -0700

    white space cleanup

--- pong.py
+++ pong.py
@@ -25,7 +25,6 @@ paddle2_vel = 0.0
 paddle_speed = 200 / 60
 max_fps = 69
 
-
 def spawn_ball(direction):
     global paddle1_pos, paddle2_pos, paddle1_vel, paddle2_vel, ball_pos, ball_vel
     paddle1_pos = HEIGHT/2
@@ -34,7 +33,7 @@ def spawn_ball(direction):
     paddle2_vel = 0.0
     ball_pos = [WIDTH/2, HEIGHT/2]
     ball_vel = [random.randrange(120, 240)/60.0 * direction, random.randrange(60, 180)/60.0]
-    
+
 # define event handlers
 def new_game():
     global paddle1_pos, paddle2_pos, paddle1_vel, paddle2_vel, ball_pos, ball_vel
@@ -66,10 +65,10 @@ def keyup(key):
         paddle2_vel = 0
     elif key == simplegui.KEY_MAP["down"]:
         paddle2_vel = 0
-    
+
 def draw(canvas):
     global score1, score2, paddle1_pos, paddle2_pos, ball_pos, ball_vel, max_fps
-    
+
     # draw mid line and gutters
     canvas.draw_line([WIDTH / 2, 0],[WIDTH / 2, HEIGHT], 1, "White")
     canvas.draw_line([PAD_WIDTH, 0],[PAD_WIDTH, HEIGHT], 1, "White")
@@ -110,42 +109,38 @@ def draw(canvas):
     elif ball_pos[1] >= HEIGHT - 1 -BALL_RADIUS:
        ball_pos[1] = HEIGHT - 1 -BALL_RADIUS
        ball_vel[1] = - ball_vel[1]
-        
+
     # update ball
     ball_pos[0] += ball_vel[0];
     ball_pos[1] += ball_vel[1];
-            
+ 
     # draw ball
     canvas.draw_circle(ball_pos, BALL_RADIUS, 2, "Red", "White")
 
-    
     # update paddle's vertical position, keep paddle on the screen
-    
-    
+
     #autopilot: paddle1_pos = ball_pos[1]
     if(paddle1_pos - HALF_PAD_HEIGHT + paddle1_vel >= 0) and (paddle1_pos + HALF_PAD_HEIGHT + paddle1_vel <= HEIGHT):
         paddle1_pos += paddle1_vel
-    
+
     #autopilot: paddle2_pos = ball_pos[1]
     if(paddle2_pos - HALF_PAD_HEIGHT + paddle2_vel >= 0) and (paddle2_pos + HALF_PAD_HEIGHT + paddle2_vel <= HEIGHT):
         paddle2_pos += paddle2_vel
-    
+
     # draw paddles
     canvas.draw_polygon([[1, paddle1_pos-HALF_PAD_HEIGHT], [PAD_WIDTH, paddle1_pos-HALF_PAD_HEIGHT], [PAD_WIDTH, paddle1_pos+HALF_PAD_HEIGHT], [0, paddle1_pos+HALF_PAD_HEIGHT]], 1, "#fff", "#cfd")
     canvas.draw_polygon([[WIDTH, paddle2_pos-HALF_PAD_HEIGHT], [WIDTH-PAD_WIDTH, paddle2_pos-HALF_PAD_HEIGHT], [WIDTH-PAD_WIDTH, paddle2_pos+HALF_PAD_HEIGHT], [WIDTH, paddle2_pos+HALF_PAD_HEIGHT]], 1, "#fff", "#fcd")
-    
-    
+
     # draw scores
     canvas.draw_text(str(score1), [150, 60], 45, "White", "sans-serif")
     canvas.draw_text(str(score2), [450, 60], 45, "White", "sans-serif")
-        
+
 # create frame
 frame = simplegui.create_frame("Pong", WIDTH, HEIGHT)
 frame.set_draw_handler(draw)
 frame.set_keydown_handler(keydown)
 frame.set_keyup_handler(keyup)
 frame.add_button("Restart", new_game)
-
 
 # start frame
 frame.start()
