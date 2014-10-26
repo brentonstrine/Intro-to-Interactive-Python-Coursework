791e4f51d4fdbc3837b2fe36c8864e8b60b33346
Brenton Strine <brenton.strine@gmail.com>
2014-10-26 00:17:37 -0700

    Beginning with pseudocode
    http://www.codeskulptor.org/#user38_3a8Qp4d63N_1.py

--- memory.py
+++ memory.py
@@ -3,19 +3,46 @@
 import simplegui
 import random
 
+
+guesses = []
+cards = []
+guessed = []
+
 # helper function to initialize globals
 def new_game():
-    pass  
+    #guesses, cards, guessed zeroed
+    #cards = range(8), concat to self.randomize order
+    
+    pass
+    
+    
+def evaluate_match():
+    #if not flipped && not current
+        # if guesses.len() = 1, compare
+        #    if match, return match, else return false. 
+        #if guesses.len() = 0, move on
+        #if guesses.len=2, erase guesses
+    # else unflip
+ pass
+    
 
      
 # define event handlers
 def mouseclick(pos):
-    # add game state logic here
+    # which card was clicked. 
+    # guesses . append()
+    # evaluate-match()
+    #if match is true, add cards to list of ignore
+    #if guesses.len() < total, you won game
     pass
     
                         
 # cards are logically 50x100 pixels in size    
 def draw(canvas):
+    #draw lines
+    
+    #draw flipped cards
+    
     pass
 
 
